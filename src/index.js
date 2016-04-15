72ec76d8a3dd65a81540fc9a17c9b889a71344f4
ben-dalton <benadalton@gmail.com>
2016-04-15 13:42:03 -0400

    add videos and send term to api

--- src/index.js
+++ src/index.js
@@ -1,10 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
+import VideoDetail from './components/video_detail';
 const API_KEY = "AIzaSyAjTJv0gBorABVgTMQnMT1ltq0cVsnyytM";
 
-const App = () => {
-	return <div>Hi</div>
+
+class App extends Component {
+	constructor(props){
+		super(props);
+
+		this.state = { 
+			videos: [],
+			selectedVideo: null
+		};
+
+		this.videoSearch('surfboards');
+	}
+
+	videoSearch(term){
+		YTSearch({ key: API_KEY, term: term }, (videos) => {
+			this.setState({ 
+				videos: videos,
+				selectedVideo: videos[0]
+			})
+		});
+	}
+
+	onVideoSelect(selectedVideo) {
+		this.setState({ selectedVideo });
+	}
+
+	render (){
+		return (
+			<div>
+				<SearchBar onSearchTermChange={term => this.videoSearch(term)} />
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList 
+					videos={this.state.videos}
+					onVideoSelect={this.onVideoSelect.bind(this)} />
+			</div>
+		);
+	};
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
